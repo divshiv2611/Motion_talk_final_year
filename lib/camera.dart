252f563a2c4487e810226f809ed191f883e00bbd
Asus <divyanshsinghds2611@gmail.com>
2024-05-23 19:25:26 +0530

    ML integration.

--- lib/camera.dart
+++ lib/camera.dart
@@ -10,6 +10,92 @@ class MyCamera extends StatefulWidget {
 class _MyCameraState extends State<MyCamera> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Motion Talk'),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 12,
+              ),
+              Card(
+                elevation: 20,
+                clipBehavior: Clip.hardEdge,
+                child: SizedBox(
+                  width: 300,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 18,
+                        ),
+                        Container(
+                          height: 280,
+                          width: 280,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12),
+                            image: const DecorationImage(image: AssetImage('assets/upload.jpg'))
+                          ),
+                          child:const Text(''),
+                        ),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        const Padding(
+                            padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text("Label",
+                              style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,
+                            ),),
+                            const SizedBox(
+                              height: 12,
+                            ),
+                            Text("Accuracy is 90%",
+                              style: TextStyle(fontSize: 18,
+                              ),),
+                            const SizedBox(
+                              height: 12,
+                            ),
+                          ],
+                        ),),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              ElevatedButton(onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 10),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(13),
+                    ),
+                    foregroundColor: Colors.black
+                  ),
+                  child: const Text("Open Camera")),
+              const SizedBox(
+                height: 8,
+              ),
+              ElevatedButton(onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(13),
+                      ),
+                      foregroundColor: Colors.black
+                  ),
+                  child: const Text("Upload From Gallery")),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
